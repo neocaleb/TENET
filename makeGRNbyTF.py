cfaf3d864c64a019785795e102c402fa0bda51ec
neocaleb <neocaleb@gmail.com>
2020-11-26 10:33:49 +0100

    version2.4

--- makeGRNbyTF.py
+++ makeGRNbyTF.py
@@ -39,7 +39,7 @@ TEzscore=(TE-numpy.mean(TE))/numpy.std(TE)
 TEpvalue=1-scipy.stats.norm.cdf(TEzscore)
 TEfdr=statsmodels.sandbox.stats.multicomp.multipletests(TEpvalue,alpha=0.05,method='fdr_bh')
 
-fdrCutoff=float(sys.argv[1])
+fdrCutoff=float(sys.argv[2])
 ofile = open(file_name.replace(".txt",".byGRN.fdr")+str(fdrCutoff)+".sif","w")
 for i in range(len(source)):
     if TEfdr[1][i]<fdrCutoff:
